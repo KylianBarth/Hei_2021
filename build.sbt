fcbdc7375fdb6891a735f35d437d359c163a7a0c
mr59 <Kralboubouk59>
2021-10-05 10:50:10 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Hei_2021"
+
+version := "0.1"
+
+scalaVersion := "2.13.0"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % "test"
---
+++ src/test/scala/FunctionsTestEx.scala
@@ -0,0 +1,50 @@
+import com.hei.fp.exercices.FunctionsExercices
+import org.scalatest.freespec.AnyFreeSpec
+
+
+class FunctionsTestEx extends AnyFreeSpec{
+  "My function  " - {
+    ": sumOfQuare for 3, 2+2 " - {
+      "should return 25" in {
+        assert( ??? == ???)
+      }
+    }
+  }
+  "My function  " - {
+    ": cube() for 3 " - {
+      "should return 27" in {
+        assert(??? == ???)
+      }
+    }
+    "addThree(Int, f(Int)) for 3 and anonymous x + 2 given " - {
+      "should return 1 + (3+1) = " in {
+        val fnt = new FunctionsExercices()
+        assert(??? == ???)
+      }
+    }
+    "factoriel(3) " - {
+      "should return 3*2*1 = 6 " in {
+        val fnt = new FunctionsExercices()
+        assert(??? == ???)
+      }
+    }
+
+    "My function  " - {
+      ": foldSum for 3 " - {
+        "should return 27" in {
+          val l = List(1, 1, 3, 5, 8)
+          assert(??? == ???)
+        }
+      }
+    }
+    // todo write tests for sumInts, sumSquares & sumPowerOfTwo
+    "My function  " - {
+      ": " - {
+        "should " in {
+          assert(??? == ???)
+        }
+      }
+    }
+  }
+}
+
